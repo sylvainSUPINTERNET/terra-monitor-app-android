af105357e0fd85b5095b67c7d1022b4a7a7dd06d
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-02-28 23:33:10 +0100

    chore(BLE): list sensor detected

--- app/src/main/java/com/pricetracker/terramonitor/ui/components/BLE/ScanBluetoothButton.kt
+++ app/src/main/java/com/pricetracker/terramonitor/ui/components/BLE/ScanBluetoothButton.kt
@@ -3,6 +3,10 @@ package com.pricetracker.terramonitor.ui.components.BLE
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -10,15 +14,18 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -30,14 +37,18 @@ import androidx.core.content.ContextCompat
 fun ScanBluetoothButton() {
     val context = LocalContext.current
 
-    val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-    val bluetoothAdapter = bluetoothManager.getAdapter()
-    var isBluetoothEnabled by remember { mutableStateOf(bluetoothAdapter?.isEnabled ?: false) }
+    val bleItemsList = remember { mutableStateListOf<String>() }
 
+    val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    val bluetoothAdapter = bluetoothManager.adapter
+    val bluetoothScanner = bluetoothManager.adapter.bluetoothLeScanner
+
+    var isBluetoothEnabled by remember { mutableStateOf(bluetoothAdapter?.isEnabled ?: false) }
     val requiredPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
         listOf(
             Manifest.permission.BLUETOOTH_SCAN,
-            Manifest.permission.BLUETOOTH_CONNECT
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.ACCESS_FINE_LOCATION
         )
     } else {
         listOf(Manifest.permission.ACCESS_FINE_LOCATION) // Scan BLE for Android <12
@@ -73,6 +84,26 @@ fun ScanBluetoothButton() {
     }
 
 
+    val callbackBle = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            super.onScanResult(callbackType, result)
+            Log.d("ScanCallback-BLE", "onScanResult: $result")
+
+            val deviceName = if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+                result?.device?.name?: "Unknown Device"
+            } else {
+                "Permission Denied to access device name"
+            }
+
+            if ( !bleItemsList.contains(deviceName) ) {
+               bleItemsList.add(deviceName);
+            }
+            //val toast = Toast.makeText(context, "onScanResult: $result", Toast.LENGTH_LONG)
+            //toast.show()
+        }
+    }
+
+
 
     if ( !allPermissionsGranted ) {
 
@@ -102,6 +133,20 @@ fun ScanBluetoothButton() {
             }
         }
 
+        val filters: List<ScanFilter> = emptyList()
+//            listOf(
+//                ScanFilter.Builder()
+//                    .setDeviceName("TERRA-IOT")
+//                    .build()
+//            )
+
+        val settings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+        Log.d("ScanCallback-BLE", "startScan ...")
+        Log.d("ScanCallback-BLE", bluetoothScanner.toString());
+        bluetoothScanner.startScan(filters, settings, callbackBle) // BLE scan
+
         if ( !isBluetoothEnabled ) {
             Column {
                 Button(
@@ -113,6 +158,12 @@ fun ScanBluetoothButton() {
                     }
                 ) {
                     Text("Search Terra Monitor ...")
+                }
+            }
+        } else {
+            LazyColumn() {
+                items(bleItemsList) {
+                    item -> Text(text = item)
                 }
             }
         }
