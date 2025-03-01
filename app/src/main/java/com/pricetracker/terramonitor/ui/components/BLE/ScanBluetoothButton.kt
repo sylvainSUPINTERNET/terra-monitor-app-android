be77a74f6e6ca76e5323561244c913d93126120d
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-03-01 23:47:41 +0100

    WIP

--- app/src/main/java/com/pricetracker/terramonitor/ui/components/BLE/ScanBluetoothButton.kt
+++ app/src/main/java/com/pricetracker/terramonitor/ui/components/BLE/ScanBluetoothButton.kt
@@ -16,15 +16,27 @@ import android.os.Build
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -37,16 +49,32 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
+import com.pricetracker.terramonitor.R
 
 
 @Composable
 fun ScanBluetoothButton(modifier: Modifier) {
     val context = LocalContext.current
+
+//    val gradientBrush = Brush.radialGradient(
+//        colors = listOf(
+//            Color(0xFF833AB4), // Violet foncé
+//            Color(0xFF6200EE), // Violet moyen
+//            Color(0xFFB5179E)  // Rose violacé
+//        )
+//    )
 
     val bleItemsList = remember { mutableStateListOf<String>() }
 
@@ -160,17 +188,83 @@ fun ScanBluetoothButton(modifier: Modifier) {
         Log.d("ScanCallback-BLE", bluetoothScanner.toString());
         bluetoothScanner.startScan(filters, settings, callbackBle) // BLE scan
 
+
+
         if ( !isBluetoothEnabled ) {
-            Column {
-                Button(
-                    onClick = {
-                        if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled) {
-                            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                            enableBluetoothLauncher.launch(enableBtIntent)
+
+            val pulseAnimation = remember { mutableStateOf(true) }
+            val animatedScale by animateFloatAsState(
+                targetValue = if (pulseAnimation.value) 1.2f else 1f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(durationMillis = 1000, easing = FastOutSlowInEasing),
+                    repeatMode = RepeatMode.Reverse
+                ), label = "pulseAnimation"
+            )
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                /*
+                Box(modifier=Modifier.fillMaxWidth().fillMaxHeight(), contentAlignment = Alignment.Center) {
+                    Button(
+                        onClick = {
+                            if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled) {
+                                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                                enableBluetoothLauncher.launch(enableBtIntent)
+                            }
+                        },
+                        modifier=Modifier.background(Color.Transparent),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF6200EE), //(Violet Google Material)
+                            contentColor = Color.White
+                        )
+                    ) {
+                        Text("Search Sensors", fontWeight = FontWeight.Medium, fontSize = 16.sp)
+                    }
+                }*/
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Box(
+
+                        modifier=Modifier.shadow(
+                            elevation = 16.dp,
+                            shape = CircleShape,
+                            clip = true,
+                            ambientColor = Color(0xFF6200EE),
+                            spotColor = Color(0xFFB5179E),
+                        ).graphicsLayer(
+                            scaleX = animatedScale,
+                            scaleY = animatedScale,
+                            shadowElevation = animatedScale * 8.dp.value
+                        )
+                    ) {
+                        Button(
+                            onClick = {
+                                if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled) {
+                                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                                    enableBluetoothLauncher.launch(enableBtIntent)
+                                }
+                            },
+                            modifier = Modifier.size(128.dp).clip(CircleShape),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xFF6200EE),
+                                contentColor = Color.White
+                            )
+
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.bluetooth),
+                                contentDescription = stringResource(id = R.string.ble_icon),
+                                modifier = Modifier.size(76.dp).padding(10.dp)
+                            )
                         }
                     }
-                ) {
-                    Text("Search Terra Monitor ...")
+
                 }
             }
         } else {
