f52d1ba0a3b4b218dc2b7a33e091a3862e37c259
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-03-01 01:49:25 +0100

    fix(wip): wip

--- app/src/main/java/com/pricetracker/terramonitor/MainActivity.kt
+++ app/src/main/java/com/pricetracker/terramonitor/MainActivity.kt
@@ -13,11 +13,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
@@ -31,12 +35,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import com.pricetracker.terramonitor.ui.components.BLE.ScanBluetoothButton
 import com.pricetracker.terramonitor.ui.components.BluetoothButton
 import com.pricetracker.terramonitor.ui.components.BluetoothPermissionButton
@@ -55,13 +62,14 @@ class MainActivity : ComponentActivity() {
         bluetoothAdapter.bluetoothLeScanner;
 
         enableEdgeToEdge()
+
         setContent {
             TerraMonitorTheme(
                 darkTheme=false,
                 dynamicColor=false
             ) {
                 Scaffold(
-                    modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars)
+                    modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars).windowInsetsPadding(WindowInsets.navigationBars)
                 ) { innerPadding ->
                     Column {
                         Box(
